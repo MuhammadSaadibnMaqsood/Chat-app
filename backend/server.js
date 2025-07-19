dd4c78d951d1c2508e4c50f66cb98f14a307a682
Muhammad Saad <muhammadsaady0100@gmail.com>
2025-07-18 18:10:08 -0700

    Write some boiler plate

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+import express from 'express'
+import cors from 'cors'
+import dotenv from 'dotenv'
+
+
+dotenv.config();
+const app = express();
+const port = process.env.PORT || 5000
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/',(req,res)=>{
+    res.send('API IS WORKING!!')
+})
+
+app.listen(port,()=>{
+    console.log(`SERVER RUNNING ON PORT: ${port}`);
+    
+})
